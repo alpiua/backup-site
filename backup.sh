3f8b63de9be7aca1befc11d0a6edd9c5a02a504d
alpi-ua <swirl7@gmail.com>
2016-07-29 04:27:10 +0300

    backup.sh
    Backup to Google Drive whole prestashop site

---
+++ backup.sh
@@ -0,0 +1,42 @@
+##########################################################
+#  Backup to Google Drive Script For Prestashop by alpi  #
+##########################################################
+
+site=glamourshop.ru
+workdir=/home/backup
+sitedir=/home/www/glamourshop.ru
+mysqldb    = glamour_db
+mysqluser  = glamour_db
+mysqlpass  = Ro2DCSce
+
+echo "Делаю дамп базы данных" `date`
+
+    mysqldump -u $mysqluser -p$mysqlpass $mysqldb > $workdir/$site.sql
+    zip -rq $workdir/$site/$site.sql.zip $workdir/$site.sql
+
+    [ -f "$workdir/$site.sql" ] &&
+        echo "Файл SQL базы создан" `date` &&
+        du -h $workdir/$site.sql || echo "ОШИБКА СОЗДАНИЯ БАЗЫ SQL" `date`
+
+    rm -rf $workdir/$site.sql
+
+
+echo "Архивирую файлы магазина (без картинок)" `date`
+
+    zip -rq -x=$sitedir/cache/smarty/\* -x=$sitedir/img/p/\* -x=$sitedir/newpresta/\* $workdir/$site/$site.zip $sitedir/
+
+    [ -f "$workdir/$site/$site.zip" ] &&
+        echo "Файлы сайта заархивированы" `date` &&
+        du -h $workdir/$site/$site.zip || echo "ОШИБКА СОЗДАНИЯ ФАЙЛОВ САЙТА" `date`
+
+
+echo "Архивирую файлы изображений" `date`
+
+    zip -rq $workdir/$site/$site-img.zip $sitedir/img/p/
+
+    [ -f "$workdir/$site/$site-img.zip" ] &&
+        echo "Файлы изображений заархивированы" `date` &&
+        du -h $workdir/$site/$site-img.zip || echo "ОШИБКА СОЗДАНИЯ ИЗОБРАЖЕНИЙ" `date`
+
+
+$workdir/drive push -ignore-conflict $workdir/$site
