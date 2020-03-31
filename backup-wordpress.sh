d61748905776f77c9d517d05a60489fce32c7ce9
alpi-ua <swirl7@gmail.com>
2020-03-31 00:16:47 +0000

    Update backup-wordpress.sh

--- backup-wordpress.sh
+++ backup-wordpress.sh
@@ -9,42 +9,43 @@ mysqldb=dbname
 mysqluser=username
 mysqlpass=dbpassword
 email=error_report@email.com
+backuplog=/path/to/backup.log
 
-echo `date` "Починаю працювати"
-echo `date` "Створюю дамп бази даних"
+echo `date` "Starting..."
+echo `date` "Backuping database"
 
-    mysqldump -u $mysqluser -p$mysqlpass $mysqldb > $workdir/$site/$site.sql
+    mysqldump -u "$mysqluser" -p"$mysqlpass" "$mysqldb" > "$workdir"/"$site"/"$site.sql"
 
-    [ -f "$workdir/$site.sql" ] &&
-	du -h $workdir/$site.sql &&
-        echo `date` "Файл SQL бази створено" || echo `date` "ПОМИЛКА СТВОРЕННЯ БАЗИ SQL"
-
-echo ""
-echo `date` "Переміщую старий архів"
-    mv $workdir/$site/$site.zip $workdir/$site/$site.zip.old
+    [ -f "$workdir"/"$site.sql" ] &&
+	du -h "$workdir"/"$site".sql &&
+        echo `date` "Database backup created" || echo `date` "Failed to create a database backup"
 
 echo ""
-echo `date` "Архівую файли магазину і базу MySQL"
+echo `date` "Moving the old backup"
+    mv "$workdir"/"$site"/"$site".zip "$workdir"/"$site"/"$site".zip.old
 
-zip -rq $workdir/$site/$site.zip -x=$sitedir/wp-content/cache/min/1/\* -x=$sitedir/wp-content/cache/wp-rocket/\* $sitedir/ $workdir/$site.sql
+echo ""
+echo `date` "Archiving files and database"
+
+zip -rq "$workdir"/"$site"/"$site".zip -x="$sitedir"/wp-content/cache/min/1/\* -x="$sitedir"/wp-content/cache/wp-rocket/\* "$sitedir"/ "$workdir"/"$site".sql
     
     returncode=$?    
-    if [ $returncode -ne 0 ]; then
-	echo "Error when creating $site archieve. ZIP returned code $returncode" | mail -s 'Error creating backup [MAXMI]' -a /home/maxmi/backup/backup-mi92.log $email
-	echo `date` "Помилка створення архіву. Старий архів $workdir/$site/$site.zip.old збережено"       
-	rm -rf $workdir/$site/$site.zip
+    if [ "$returncode" -ne 0 ]; then
+	echo "Error when creating "$site" archieve. ZIP returned code $returncode" | mail -s "Error creating backup "$site"" -a "$backuplog" "$email" 
+ 	echo `date` "Error creating zip file. Oldone "$workdir"/"$site"/"$site".zip.old will stay"       
+	rm -rf "$workdir"/"$site"/"$site".zip
     else
-    [ -f "$workdir/$site/$site.zip" ] &&
-        du -h $workdir/$site/$site.zip &&
-	rm -rf $workdir/$site/$site.sql &&
-	rm -rf $workdir/$site/$site.zip.old && 
-	echo `date` "Файли сайту заархівовано. Старий архів видалено" || echo `date` "ПОМИЛКА СТВОРЕННЯ ФАЙЛІВ САЙТУ"
+    [ -f "$workdir"/"$site"/"$site".zip" ] &&
+        du -h "$workdir"/"$site"/"$site".zip &&
+	rm -rf "$workdir"/"$site"/"$site".sql &&
+	rm -rf "$workdir"/"$site"/"$site".zip.old && 
+	echo `date` "Archive created. Old archive deleted" || echo `date` "ERROR CREATING ARCHIVE"
     fi
 
 echo ""
-echo `date` "Починаю завантаження на гугл диск"
+echo `date` "Starting download to the google disk"
 
-$workdir/drive push -ignore-conflict -ignore-name-clashes -quiet $workdir/$site/$site.zip
+"$workdir"/drive push -ignore-conflict -ignore-name-clashes -quiet "$workdir"/"$site"/"$site".zip
 
-echo "Завантаження завершено"
+echo "Download complete"
 echo "`date`"
