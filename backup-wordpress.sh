e0f6bce8bd946d1f0eae7077e0a331b008e5992f
alpi-ua <swirl7@gmail.com>
2020-04-02 15:07:04 +0000

    Update backup-wordpress.sh

--- backup-wordpress.sh
+++ backup-wordpress.sh
@@ -27,42 +27,51 @@ backuplog=/path/to/backup.log
 echo $(date "$DATE_FORMAT") "| Starting..."
 echo $(date "$DATE_FORMAT") "| Backuping database"
 
-    mysqldump -u "$mysqluser" -p"$mysqlpass" "$mysqldb" > "$workdir"/"$site"/"$site".sql
-
-    [ -f ""$workdir"/"$site"/"$site".sql" ] &&
-        echo $(date "$DATE_FORMAT") "| Database backup created" || echo $(date "$DATE_FORMAT") "| Failed to create a database backup"
-
-echo $(date "$DATE_FORMAT") "| Moving the old backup if exists"
-    [ -f ""$workdir"/"$site"/"$site".zip" ] &&
-    mv "$workdir"/"$site"/"$site".zip "$workdir"/"$site"/"$site".zip.old
-
-echo $(date "$DATE_FORMAT") "| Archiving files and database"
-
-zip -rq "$workdir"/"$site"/"$site".zip -x="$sitedir"/wp-content/cache/min/1/\* -x="$sitedir"/wp-content/cache/wp-rocket/\* -x="$sitedir"/wp-content/uploads/cache/\* $workdir/$site/$site.sql "$sitedir"/ 
+mysqldump -u "$mysqluser" -p"$mysqlpass" "$mysqldb" > "$sitedir"/"$site".sql
 
     returncode=$?
 
     if [ $returncode -ne 0 ] 
     then
+	echo $(date "$DATE_FORMAT") "| Failed to create a database backup. Return code is "$returncode""
+	echo "Error backuping "$site" database. mysqldump returned code $returncode" | mail -s "Error backuping DB "$site"" -a "$backuplog" "$email"
+    else
+        echo $(date "$DATE_FORMAT") "| Database backup created" 
+    fi
+
+[ -f ""$workdir"/"$site"/"$site".zip" ] &&
+    echo $(date "$DATE_FORMAT") "| Moving the old backup to "$site".zip.old" && 
+    mv "$workdir"/"$site"/"$site".zip "$workdir"/"$site"/"$site".zip.old
+
+echo $(date "$DATE_FORMAT") "| Archiving files and database"
+
+zip -rq "$workdir"/"$site"/"$site".zip -x="$sitedir"/wp-content/cache/min/1/\* -x ="$sitedir"/wp-content/uploads/cache/\* -x="$sitedir"/wp-content/cache/wp-rocket/\* -x=/"$sitedir"/wp-content/uploads/cache/\* "$sitedir" 
+
+    returncode=$?
+
+    if [ $returncode -ne 0 ] 
+    then
+ 	echo $(date "$DATE_FORMAT") "| Error creating zip file. "$workdir"/"$site"/"$site".zip.old will stay. Return code is "$returncode""       
 	echo "Error when creating "$site" archieve. ZIP returned code $returncode" | mail -s "Error creating backup "$site"" -a "$backuplog" "$email" 
- 	echo $(date "$DATE_FORMAT") "| Error creating zip file. "$workdir"/"$site"/"$site".zip.old will stay"       
 	rm -rf "$workdir"/"$site"/"$site".zip
     else
-	rm -rf "$workdir"/"$site"/"$site".sql &&
-	rm -rf "$workdir"/"$site"/"$site".zip.old && 
+	rm -rf "$sitedir"/"$site".sql 
+	rm -rf "$workdir"/"$site"/"$site".zip.old 
 	echo $(date "$DATE_FORMAT") "| Archive created. Old archive deleted"
     fi
 
 echo $(date "$DATE_FORMAT") "| Starting upload to the google disk"
 
-"$workdir"/drive push -ignore-conflict -ignore-name-clashes -quiet "$workdir"/"$site"/"$site".zip > /dev/null
+[ -f ""$workdir"/"$site"/"$site".zip" ] &&
+	"$workdir"/drive push -ignore-conflict -ignore-name-clashes -quiet "$workdir"/"$site"/"$site".zip > /dev/null || 
+	echo $(date "$DATE_FORMAT") "| No file to upload, cancelling"
     
     returncode=$?
     
     if [ $returncode -ne 0 ]
     then
+	echo $(date "$DATE_FORMAT") "| Error occured. Upload NOT complete. Return code is "$returncode""
     	echo "Error occured while uploading archive to google" | mail -s "Error while uploading "$site" backup" -a "$backuplog" "$email"
-	echo $(date "$DATE_FORMAT") "| Error occured. Upload NOT complete"
     else
-	echo $(date "$DATE_FORMAT") "| Upload complete"
+	echo $(date "$DATE_FORMAT") "| Task complete"
     fi
